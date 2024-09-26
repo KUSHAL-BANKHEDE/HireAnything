c51e579721837f2e40c5d8ffd16b47460f8ed959
KUSHAL BANKHEDE <kushalbankhede1@gmail.com>
2024-09-26 16:35:41 +0530

    edit cors

--- Backend/index.js
+++ Backend/index.js
@@ -16,8 +16,15 @@ const FormModel = mongoose.model('Form', new mongoose.Schema({
 
 
 const app = express();
+
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  credentials: true
+}));
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
